b20ab4d7eadbebaf03709db92891cc97ba3b0245
Kaiden Kennedy <103951147+kaidenkennedy7@users.noreply.github.com>
2022-10-10 15:49:25 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Carousel from "./Carousel";
+import CoinContainer from "./CoinContainer";
 
 function App() {
   return (
     <div className="App">
-      <Carousel />
+      <CoinContainer />
     </div>
   );
 }
